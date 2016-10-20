3399145660b1af0e70e32dd088653d0ca61573e5
abarla <haritha.manchili@gmail.com>
2016-10-19 20:33:34 -0500

    commit

--- src/test/java/com/codeforanyone/edujavajsp/Start.java
+++ src/test/java/com/codeforanyone/edujavajsp/Start.java
@@ -60,8 +60,7 @@ public class Start {
 			server.stop();
 
 			// The use of server.join() the will make the current thread join
-			// and wait until the server is done executing. See
-			// http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
+			// and wait until the server is done executing.
 			server.join();
 		} catch (Exception e) {
 			// basic error reporting to console.
